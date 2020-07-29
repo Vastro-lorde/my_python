92c5152fc863423ca3cd34a1eed58616f796bf7e
Seun Daniel Omatsola <omatsolaseund@gmail.com>
2020-07-29 12:21:11 +0000

    Add files via upload

---
+++ home.py
@@ -0,0 +1,23 @@
+'''
+ num1 = int(input("enter the first number : "))
+ num2 = int(input("enter the second number : "))
+ num3 = int(input("enter the third number : "))
+ result = num1 + num2 + num3
+ print(result)
+ '''
+'''
+print("****Answer Yes OR No****")
+response = input("Are you from Nigerian? ")
+print(response)
+
+if response == "yes" :
+    print("You are qualified for paliatives.")
+elif response == "no" :
+    print("You are not qualified for Paliatives.")
+else:
+    print("Run commot from here Now thief.")
+'''
+names_of_students = ["Daniel", "Kirito", "Vast", "Uzumaki", "Ichigo", "Spirit"]
+names_of_students[0] ="Seun"
+names_of_students.append("Lisa")
+print(names_of_students)
